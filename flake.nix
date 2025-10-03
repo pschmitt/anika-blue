7a10a36c77d1f145263a0b7c79b94ad96389b744
Philipp Schmitt <philipp@schmitt.co>
2025-10-03 22:43:34 +0200

    fix DATABASE

--- flake.nix
+++ flake.nix
@@ -203,7 +203,7 @@
               echo "Run 'python app.py' to start the development server"
               echo "Run 'nix build .#docker' to build Docker image"
               echo
-              export DATABASE="${"DATABASE:-$PWD/anika_blue.db"}"
+              export DATABASE="''${DATABASE:-$PWD/anika_blue.db}"
               export FLASK_APP="$PWD/app.py"
             '';
           };
